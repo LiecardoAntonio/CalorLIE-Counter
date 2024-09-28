83ee62f8d4116e8725dc07c3e40311c8ca2c090d
LiecardoAntonio <liecardoantonio24@gmail.com>
2024-09-28 08:40:28 +0700

    progress

--- script.js
+++ script.js
@@ -65,7 +65,13 @@ function addEntry() {
 
   //3
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-  const entryNumber = targetInputContainer.querySelectorAll();
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories" >`; //store literals sting of a HTML element (Label and Input).
 }
 
 
